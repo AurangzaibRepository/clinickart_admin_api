d2e0c6fc026b9b27e840368c86c96147f9e138ca
Aurangzaib <aurangzaib_najam@hotmail.com>
2026-08-03 22:50:54 +0500

    Implements metrics interceptor

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
+import { MetricsInterceptor } from './common/interceptors/metrics.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useStaticAssets(join(__dirname, '..', 'uploads'), {
     prefix: '/uploads',
   });
+
+  app.useGlobalInterceptors(new MetricsInterceptor());
 
   await app.listen(process.env.PORT ?? 3000);
 }
