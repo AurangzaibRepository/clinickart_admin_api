0c86d0bd5dc095050d9b1d96148e22af2bff55d6
Aurangzaib <aurangzaib_najam@hotmail.com>
2026-09-17 10:32:32 +0500

    Installss wager

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe, ClassSerializerInterceptor } from '@nestjs/common';
 import { NestFactory, Reflector } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { join } from 'path';
 import { AppModule } from './app.module';
 import { MetricsInterceptor } from './common/interceptors/metrics.interceptor';
@@ -26,6 +27,16 @@ async function bootstrap() {
     new ResponseInterceptor(),
   );
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Clinickart Admin API')
+    .setDescription('Clinickart Admin API documentation')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api/docs', app, document);
 
   await app.listen(process.env.APP_PORT ?? 3000);
 }
